1f9e4d2b7b51323101fe7d86821c26c82b7769cf
Dan Fairaizl <fairaizl@gmail.com>
2019-04-17 10:10:48 -0400

    Build unused dns verification check for api callbacks

--- server/lib/tasks.js
+++ server/lib/tasks.js
@@ -1,14 +1,39 @@
+const { Resolver } = require('dns').promises;
 const fetch = require('node-fetch');
+const {URL} = require('url');
+
+const resolver = new Resolver();
+
+function verifyTxt (records) {
+  return Promise.resolve();
+}
 
 function runHTTPtask (task) {
   // verify remote server
+  const url = new URL(task.uri);
+  const hostname = url.host;
+  console.log('Resolving DNS for', hostname);
+  // return resolver.resolveTxt(hostname)
+  //   .then(verifyTxt())
+  //   .then(() => {
+  //     // then hit the endpoint
+  //     return fetch(task.uri, { method: 'POST', data: task.payload })
+  //       .then((res) => {
+  //         if (res.ok) {
+  //           console.log('Success from remote host webhook');
+  //         }
+  //       });
+  //   });
 
-  // then hit the endpoint
-  return fetch(task.uri, { method: 'POST', data: task.payload })
-    .then((res) => {
-      if (res.ok) {
-        console.log('Success from remote host webhook');
-      }
+  return verifyTxt()
+    .then(() => {
+      // then hit the endpoint
+      return fetch(task.uri, { method: 'POST', data: task.payload })
+        .then((res) => {
+          if (res.ok) {
+            console.log('Success from remote host webhook');
+          }
+        });
     });
 }
 
